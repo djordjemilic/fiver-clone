1a99084d6c3d497255c046dccfb0ad7364318262
Djordje <djordjemilic33@gmail.com>
2023-02-23 14:49:59 +0100

    add: trusted by section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 import Featured from "../../components/featured/Featured";
+import Trusted from "../../components/trusted/Trusted";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div>
       <Featured />
+      <Trusted />
     </div>
   );
 };
